f8cd31f792616a2c1b0fa102bdc5c09adb8cb14a
ceoimon <ceoimon>
2016-11-06 07:42:02 +0800

    fixed url?

--- bundle.js
+++ bundle.js
@@ -53201,7 +53201,7 @@
 	  dis: '300m',
 	  person: '刘伯伯',
 	  qrcode: 'http://video.sike.io/images/xiaosimei_qrcode.png',
-	  map: '/images/dajuyuan.png',
+	  map: 'images/dajuyuan.png',
 	  story: ['羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，但穿戴義肢的膝蓋不堪摩擦而罹患蜂窩性組織炎，因此在家休息多年。在兒子的鼓勵下，羅大哥到社區大學學習調配咖啡，開始與咖啡為伍的日子。', '也因為羅大下水道業，卻因某次工作哥的努力與堅持，自家烘焙的賞味咖啡就此誕生。羅大哥原本從事地他因為經濟因素持下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，'],
 	  comments: [{
 	    profile: 'http://writedirection.com/website/wp-content/uploads/2016/09/blank-profile-picture-973460_960_720.png',
@@ -53238,7 +53238,7 @@
 	  dis: '600m',
 	  person: '阿嬷',
 	  qrcode: 'http://video.sike.io/images/xiaosimei_qrcode.png',
-	  map: '/images/dajuyuan.png',
+	  map: 'images/dajuyuan.png',
 	  story: ['羅大哥原本從事地下水道業，卻因某次工作上的意外導致他因為經濟因素持左腿截肢。工傷的他因為經濟因素持續勞力工作，但穿戴義肢的膝蓋不堪摩擦而罹患蜂窩性組織炎，因此在家休息多年。在兒子的鼓勵下，羅大哥到社區大學學習調配咖啡，開始與咖啡為伍的日子。', '也因為羅大哥的努力與堅持，自家烘焙的賞味咖啡就此誕生。羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，'],
 	  comments: [{
 	    profile: 'https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg',
@@ -53275,7 +53275,7 @@
 	  dis: '800m',
 	  person: '陈阿姨',
 	  qrcode: 'http://video.sike.io/images/xiaosimei_qrcode.png',
-	  map: '/images/dajuyuan.png',
+	  map: 'images/dajuyuan.png',
 	  story: ['羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，但穿戴義肢的膝蓋不堪摩擦而罹患蜂窩性組織炎，因此在家休息多年。在兒子的鼓勵下，羅大哥到社區大學學習調配咖啡，開始與咖啡為伍的日子。', '也因為羅大哥的努力與堅持，自家烘焙的賞味咖啡就此誕生。羅大哥下水道業，卻因某次工作上的意外導致左腿原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，'],
 	  comments: [{
 	    profile: 'https://lh3.googleusercontent.com/--cuJjE1uaFY/AAAAAAAAAAI/AAAAAAAAAAA/4fahE7RgoKA/photo.jpg',
@@ -53312,7 +53312,7 @@
 	  dis: '1.0km',
 	  person: '罗大哥',
 	  qrcode: 'http://video.sike.io/images/xiaosimei_qrcode.png',
-	  map: '/images/dajuyuan.png',
+	  map: 'images/dajuyuan.png',
 	  story: ['羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，但穿戴義肢的膝蓋不堪摩擦而罹患蜂窩性組織炎，因此在家休息多年。在兒子的鼓勵下，羅大哥到社區大學學習調配咖啡，開始與咖啡為伍的日子。', '也因為羅大哥的努力與堅持，自家烘焙從事地下水道業，卻因某次工作上的意外導致左腿的賞味咖啡就此誕生。羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，'],
 	  comments: [{
 	    profile: 'https://lh3.googleusercontent.com/--cuJjE1uaFY/AAAAAAAAAAI/AAAAAAAAAAA/4fahE7RgoKA/photo.jpg',
@@ -53349,7 +53349,7 @@
 	  dis: '1.2km',
 	  person: '陈阿姨',
 	  qrcode: 'http://video.sike.io/images/xiaosimei_qrcode.png',
-	  map: '/images/dajuyuan.png',
+	  map: 'images/dajuyuan.png',
 	  story: ['羅大哥原本從事地下他因為經濟因素持導致左腿截肢。工傷的他因為他因為經濟因素持經濟因素持續勞力工作，但穿戴義肢的膝蓋不堪摩擦而罹患蜂窩性組織炎，因此在家休息多年。在兒子的鼓勵下，羅大哥到社區大學學習調配咖啡，開始與咖啡為伍的日子。', '也因為羅大哥的努力與堅持，自家烘焙的賞味咖次工作上的意外啡就此誕生。羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，'],
 	  comments: [{
 	    profile: 'https://lh3.googleusercontent.com/--cuJjE1uaFY/AAAAAAAAAAI/AAAAAAAAAAA/4fahE7RgoKA/photo.jpg',
@@ -53386,7 +53386,7 @@
 	  dis: '1.5km',
 	  person: '阿嬷',
 	  qrcode: 'http://video.sike.io/images/xiaosimei_qrcode.png',
-	  map: '/images/dajuyuan.png',
+	  map: 'images/dajuyuan.png',
 	  story: ['下水道業，卻因某次工作羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿下水道業，卻因某次工作截肢。工傷的他因為經濟因素持續勞力工作，但穿戴義肢的膝蓋不堪摩擦而罹患蜂窩性組織炎，因此在家休息多年。在兒子的鼓勵下，羅大哥到社區大學學習調配咖啡，開始與咖啡為伍的日子。', '也因為羅大哥的努力與堅持，自家烘焙的賞味咖啡就此誕生。羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，羅大哥原本從事地下水道業，卻因某次工作上的意外導致左腿截肢。工傷的他因為經濟因素持續勞力工作，'],
 	  comments: [{
 	    profile: 'https://lh3.googleusercontent.com/--cuJjE1uaFY/AAAAAAAAAAI/AAAAAAAAAAA/4fahE7RgoKA/photo.jpg',
